c014437663c2d3204024b43b5d7206e9958832d7
manish kumar <manishagrawal1994@gmail.com>
2018-08-30 14:25:22 +0530

    first commit

---
+++ library/redisLib.js
@@ -0,0 +1,65 @@
+//redis Lab
+const check = require("./checkLib");
+const redis = require("redis");
+let client = redis.createClient();
+
+client.on('connect', () => {
+
+    console.log("Redis connection successfully opened");
+
+});
+
+client.on('connect',() => {
+    console.log("Redis connection successfully opened");
+});
+
+let getAllUsersInAHash = (hashName, callback) => {
+    
+    client.HGETALL(hashName, (err, result) => {
+        console.log(`Getting all online users from hash ${hashName}`);
+
+        if(err){
+            console.log(err);
+            callback(err, null);
+        }
+        else if(check.isEmpty(result)){
+            console.log("online user list is empty");
+            console.log(result);
+            callback(null,{});
+        }
+        else{
+            console.log(result);
+            callback(null,result);
+        }
+    });//end HGETALL
+}//end getAllUsersInAHash
+
+let setANewOnlineUserInHash = (hashName, key, value, callback) => {
+
+    console.log(`setting user ${key} with value ${value} in hash ${hashName}`);
+
+    client.HMSET( hashName, [
+        key, value
+    ], (err, result) => {
+        if(err){
+            console.log(err);
+            callback(err,null);
+        }else{
+            console.log("user has been set in hash map");
+            console.log(result);
+            callback(null, result);
+        }
+    });//end HMSET
+}//end set a new online user in hash
+
+let deleteUserFromHash = (hashName,key) => {
+
+    client.HDEL(hashName,key);
+    return true;
+}//end delete user from hash
+
+module.exports = {
+    getAllUsersInAHash: getAllUsersInAHash,
+    setANewOnlineUserInHash: setANewOnlineUserInHash,
+    deleteUserFromHash: deleteUserFromHash
+}
---
+++ models/ChatRoomModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const time = require('./../library/timeLib');
+
+//roomid=receiverid
+const ChatRoom = new Schema({
+    roomId: {
+        type: String,
+        default: 0,
+        index: true,
+        unique: true
+    },
+    roomName: {
+        type: String,
+        default: "New Group"
+    },
+
+    creator: {
+        type: String,
+        default: ''
+      },
+    admin: {},
+    createdOn: {
+        type: Date,
+        default: time.now
+    },
+    members:[],
+    status:{
+        type: Boolean,
+        default: true
+    } 
+});
+
+mongoose.model('ChatRoomModel', ChatRoom);
